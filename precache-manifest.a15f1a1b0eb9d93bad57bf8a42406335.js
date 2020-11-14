94cfe76857c91e83a3f7cc6735133464bdb76a23
tag-ap <test.action@yahoo.com.com>
2020-11-14 16:26:44 +0300

    Updates

---
+++ precache-manifest.a15f1a1b0eb9d93bad57bf8a42406335.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8ae91a8bc14020a957663e6543ba0e2",
+    "url": "/tag-app/index.html"
+  },
+  {
+    "revision": "b77b0502bfad26d4f09f",
+    "url": "/tag-app/static/css/0.52c23fd6.chunk.css"
+  },
+  {
+    "revision": "adaf981d2381671452cd",
+    "url": "/tag-app/static/css/3.5afb53b0.chunk.css"
+  },
+  {
+    "revision": "99660b8d1c06d7b765cc",
+    "url": "/tag-app/static/css/5.6ede3073.chunk.css"
+  },
+  {
+    "revision": "c2c9a03079eb2e19ffca",
+    "url": "/tag-app/static/css/7.792a87e0.chunk.css"
+  },
+  {
+    "revision": "5fa735cd6dba13eb093e",
+    "url": "/tag-app/static/css/main.72b7d6a8.chunk.css"
+  },
+  {
+    "revision": "b77b0502bfad26d4f09f",
+    "url": "/tag-app/static/js/0.7334393d.chunk.js"
+  },
+  {
+    "revision": "adaf981d2381671452cd",
+    "url": "/tag-app/static/js/3.08ed6890.chunk.js"
+  },
+  {
+    "revision": "d6828686dc8dee571612",
+    "url": "/tag-app/static/js/4.d80f3bbf.chunk.js"
+  },
+  {
+    "revision": "99660b8d1c06d7b765cc",
+    "url": "/tag-app/static/js/5.b32c842e.chunk.js"
+  },
+  {
+    "revision": "b7ac5996ecae96f32a90",
+    "url": "/tag-app/static/js/6.0ef3d87d.chunk.js"
+  },
+  {
+    "revision": "c2c9a03079eb2e19ffca",
+    "url": "/tag-app/static/js/7.cff0cdb1.chunk.js"
+  },
+  {
+    "revision": "5fa735cd6dba13eb093e",
+    "url": "/tag-app/static/js/main.4235f39e.chunk.js"
+  },
+  {
+    "revision": "d3f23897ebfd94d20e56",
+    "url": "/tag-app/static/js/runtime~main.b6db6468.js"
+  },
+  {
+    "revision": "cbb4eead04871188de6ce488808e4237",
+    "url": "/tag-app/static/media/bubble-tail-left.cbb4eead.svg"
+  },
+  {
+    "revision": "b159f8b04a1646f68bfc0bfb7f347cac",
+    "url": "/tag-app/static/media/bubble-tail-right.b159f8b0.svg"
+  },
+  {
+    "revision": "bcf112461b322d3b888e4eb550e3386a",
+    "url": "/tag-app/static/media/camomile_blurred.bcf11246.jpg"
+  },
+  {
+    "revision": "71da74694e314a0e3a855ded564a1eaf",
+    "url": "/tag-app/static/media/check.71da7469.svg"
+  },
+  {
+    "revision": "ac1331a490a116a48daa6c9f41b6db80",
+    "url": "/tag-app/static/media/telegram-logo.ac1331a4.svg"
+  }
+]);
